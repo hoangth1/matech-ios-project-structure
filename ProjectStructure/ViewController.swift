e2fca384b98a60622b6f1a2cdcae8067546a3b20
hoangth1 <thanghuyhoanghust@gmail.com>
2021-10-05 15:12:04 +0700

    Init workspace

---
+++ ProjectStructure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectStructure
+//
+//  Created by M1 on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
